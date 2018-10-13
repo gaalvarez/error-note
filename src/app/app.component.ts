ef970f8cbb1a74c98ab0db44fdd22f1d27defa30
Gustavo Álvarez <gaalvarez0910@gmail.com>
2018-10-13 07:17:43 -0500

    primer commit, clase 1 hasta arquitectura

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
   title = 'error-note';
+  count = 0;
+
+  ngOnInit(): void {
+    console.log('Hello Angular from console');
+  }
+
+  increment() {
+    this.count++;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { Routes, RouterModule } from '@angular/router';
+import { LoggerService } from './core/logger/logger.service';
+import { ConsoleLoggerService } from './core/logger/console-logger.service';
+
+const routes: Routes = [
+  { path: 'notes', loadChildren: './notes/notes.module#NotesModule' }
+];
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
-  providers: [],
+  providers: [{ provide: LoggerService, useClass: ConsoleLoggerService }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
