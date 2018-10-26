2b150d61126599b5e2ebae117b5cd808108acd89
Gustavo Álvarez <gaalvarez0910@gmail.com>
2018-10-26 16:51:31 -0500

    avance hasta la sección de formularios reactivos

--- src/app/notes/notes.module.ts
+++ src/app/notes/notes.module.ts
@@ -4,13 +4,13 @@ import { CommonModule } from '@angular/common';
 import { RegisterComponent } from './pages/register/register.component';
 import { SearchComponent } from './pages/search/search.component';
 import { CommentComponent } from './pages/comment/comment.component';
-import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   imports: [
     CommonModule,
     NotesRoutingModule,
-    FormsModule
+    ReactiveFormsModule
   ],
   declarations: [RegisterComponent, SearchComponent, CommentComponent]
 })
--- src/app/notes/pages/register/register.component.ts
+++ src/app/notes/pages/register/register.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { LoggerService } from 'src/app/core/logger/logger.service';
 import { UserRegister } from '../../shared/user-register';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -9,6 +10,18 @@ import { UserRegister } from '../../shared/user-register';
 })
 export class RegisterComponent implements OnInit {
 
+  registerForm = this.fb.group({
+    name: ['', Validators.required],
+    lastname: [''],
+    username: ['', Validators.required],
+    email: [
+      '',
+      [Validators.required, Validators.email]
+    ],
+    birthdate: [''],
+    gender: ['']
+  });
+  errors = { required: 'el valor es requerido', email: 'Formato de email inválido' };
   genders = ['Femenino', 'Masculino', 'Otro'];
   user: UserRegister = {
     name: '',
@@ -18,11 +31,25 @@ export class RegisterComponent implements OnInit {
     gender: this.genders[1],
     birthdate: new Date(),
   };
-
-  constructor(private log: LoggerService) { }
+  constructor(private fb: FormBuilder, private log: LoggerService) { }
 
   ngOnInit() {
+  }
 
+  hasError(control: string): boolean {
+    if (this.registerForm.controls[control].errors) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  messageError(control: string): string {
+    if (this.registerForm.controls[control].errors) {
+      return this.errors[Object.keys(this.registerForm.controls[control].errors)[0]];
+    } else {
+      return '';
+    }
   }
 
 }
